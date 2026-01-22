4ebf19a4e9f5bcf7537362d9d1b1ee267d2a7fd2
sarajamal <sarajml806@gmail.com>
2026-01-22 17:00:28 +0100

    Added navbar and started dashboard

--- src/app/User/navbar/navbar.ts
+++ src/app/User/navbar/navbar.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  isDarkMode: boolean = false;
 
+  toggleTheme() : void {
+    this.isDarkMode = !this.isDarkMode;
+  }
 }
